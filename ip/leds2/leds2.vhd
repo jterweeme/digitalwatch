bade89aa422d4687d29a7bbf614cff32dd9ced26
Jasper ter Weeme <jasper@nbalternate.lan>
2014-03-14 21:52:38 +0100

    backup

--- ip/leds2/leds2.vhd
+++ ip/leds2/leds2.vhd
@@ -4,14 +4,19 @@ use ieee.numeric_std.all;
 
 entity leds2 is
     port (clk, reset, slave_write: in std_logic;
-          slave_address: in std_logic_vector[3:0];
-          slave_writedata: in std_logic_vector[7:0];
-          user_dataout_0: out std_logic_vector[7:0]);
-)
+          slave_address: in std_logic_vector(3 downto 0);
+          slave_writedata: in std_logic_vector(7 downto 0);
+          user_dataout_0: out std_logic_vector(7 downto 0));
 end leds2;
 
 architecture behavior of leds2 is
 begin
-    
+    ledjes: process(clk, reset) begin
+        if reset = '1' then
+            user_dataout_0 <= (others => '0');
+        elsif slave_write = '1' then
+            user_dataout_0 <= slave_writedata;
+        end if;
+    end process;
 end behavior;
 
