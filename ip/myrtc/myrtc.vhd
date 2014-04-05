fcfa63b465b906aadda9795a699e0052619b0894
Jasper ter Weeme <jasper@nbalternate.lan>
2014-04-05 02:20:01 +0200

    backup

---
+++ ip/myrtc/myrtc.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity myrtc is
+    port (clk, reset: in std_logic);
+end myrtc;
+
+architecture behavior of myrtc is
+begin
+    
+end behavior;
+
+
