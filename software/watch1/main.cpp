e78d0b579a979a1fe54e3d7819e23204764e61b7
Jasper ter Weeme <jasper@nbalternate.lan>
2014-04-08 03:22:31 +0200

    backup

--- software/watch1/main.cpp
+++ software/watch1/main.cpp
@@ -19,8 +19,55 @@ whilst being set.
 #include <unistd.h>
 #include <io.h>
 #include <sys/alt_irq.h>
-#include "main.h"
 #include "buttons.h"
+#include "uart.h"
+#include "timer.h"
+#include "segment.h"
+#include "rtc.h"
+
+class Leds
+{
+public:
+    Leds();
+    void write(uint8_t);
+private:
+    volatile uint8_t *handle;
+};
+
+/* State Pattern */
+class AbstractMode
+{
+public:
+    virtual void increase() {};
+    virtual void timerTick() {};
+};
+
+class Watch
+{
+public:
+    TimeDisplay *getTimeDisplay();
+    Leds *getLeds();
+    RTC *getRTC();
+    Uart *getUart();
+    Terminal *getDebugger();
+    void nextMode();
+    void increment();
+    void timerTick();
+    void init();
+private:
+    Uart *uart;
+    Leds *leds;
+    Buttons *buttons;
+    TimeDisplay *segDisplay;
+    Timer *timer;
+    RTC *rtc;
+    Terminal *debugger;
+    static const uint8_t DISPLAY_TIME_MODE = 1;
+    static const uint8_t INCREMENT_HOURS_MODE = 2;
+    static const uint8_t INCREMENT_MINUTES_MODE = 3;
+    uint8_t mode;
+    AbstractMode *mode2;
+};
 
 class DisplayTimeMode : public AbstractMode
 {
--- software/watch1/main.h
+++ software/watch1/main.h
@@ -11,49 +11,5 @@
 #include "buttons.h"
 #include "rtc.h"
 
-class Leds
-{
-public:
-    Leds();
-    void write(uint8_t);
-private:
-    volatile uint8_t *handle;
-};
-
-/* State Pattern */
-class AbstractMode
-{
-public:
-    virtual void increase() {};
-    virtual void timerTick() {};
-};
-
-class Watch
-{
-public:
-    TimeDisplay *getTimeDisplay();
-    Leds *getLeds();
-    RTC *getRTC();
-    Uart *getUart();
-    Terminal *getDebugger();
-    void nextMode();
-    void increment();
-    void timerTick();
-    void init();
-private:
-    Uart *uart;
-    Leds *leds;
-    Buttons *buttons;
-    TimeDisplay *segDisplay;
-    Timer *timer;
-    RTC *rtc;
-    Terminal *debugger;
-    static const uint8_t DISPLAY_TIME_MODE = 1;
-    static const uint8_t INCREMENT_HOURS_MODE = 2;
-    static const uint8_t INCREMENT_MINUTES_MODE = 3;
-    uint8_t mode;
-    AbstractMode *mode2;
-};
-
 #endif
 
