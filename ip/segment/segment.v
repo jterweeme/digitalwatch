5c0dfac75f61f551a781e80a77028a7afa8b73d1
Jasper ter Weeme <jasper@nbalternate.lan>
2014-04-10 22:36:36 +0200

    backup

--- ip/segment/segment.v
+++ ip/segment/segment.v
@@ -17,57 +17,43 @@ module slave_template (
 
 	parameter DATA_WIDTH = 8;
 	parameter ENABLE_SYNC_SIGNALS = 0;
-	input clk, reset;
-	
-	input [3:0] slave_address;
-	input slave_read;
-	input slave_write;
-	output reg [7:0] slave_readdata;
-	input [7:0] slave_writedata;
-	input slave_byteenable;
-
-	output wire [7:0] user_dataout_0, user_dataout_1;
-	output wire [15:0] user_chipselect;
-	output wire user_byteenable;
-	output wire user_write;
-	output wire user_read;
+	input clk, reset, slave_read, slave_write, slave_byteenable;
+    input [3:0] slave_address;
+    output reg [7:0] slave_readdata;
+    input [7:0] slave_writedata;
+    output wire [7:0] user_dataout_0, user_dataout_1;
+    output wire [15:0] user_chipselect;
+    output wire user_byteenable, user_write, user_read;
 	
     wire [7:0] user_dataout_d0, user_dataout_d1, user_dataout_d2, user_dataout_d3, user_dataout_d4,
             user_dataout_d5, user_dataout_d6, user_dataout_d7, user_dataout_d8;
 
-	wire internal_byteenable;
-	reg internal_byteenable_d1;
-	wire [15:0] address_decode;
-	reg [15:0] address_decode_d1;
-	wire [3:0] address_bank_decode;
-	reg [3:0] address_bank_decode_d1;
+    wire internal_byteenable;
+    reg internal_byteenable_d1;
+    wire [15:0] address_decode;
+    reg [15:0] address_decode_d1;
+    wire [3:0] address_bank_decode;
+    reg [3:0] address_bank_decode_d1;
     reg slave_read_d1;
     reg slave_read_d2;
     reg slave_write_d1;
-	reg [7:0] user_datain_0_d1;
-	reg [7:0] user_datain_1_d1;
-	reg [7:0] user_datain_2_d1;
-	reg [7:0] user_datain_3_d1;
-	reg [7:0] user_datain_4_d1;
-	reg [7:0] user_datain_5_d1;
-	reg [7:0] user_datain_6_d1;
-	reg [7:0] user_datain_7_d1;
-	reg [7:0] user_datain_8_d1;
-	reg [7:0] mux_first_stage_a;
-	reg [7:0] mux_first_stage_b;
-	reg [7:0] mux_first_stage_c;
-	reg [7:0] mux_first_stage_d;
+
+    reg [7:0] user_datain_0_d1, user_datain_1_d1, user_datain_2_d1, user_datain_3_d1,
+                user_datain_4_d1, user_datain_5_d1, user_datain_6_d1;
+    reg [7:0] user_datain_7_d1;
+    reg [7:0] user_datain_8_d1;
+    reg [7:0] mux_first_stage_a;
+    reg [7:0] mux_first_stage_b;
+    reg [7:0] mux_first_stage_c;
+    reg [7:0] mux_first_stage_d;
 
     reg [26:0] cnt;
     reg [7:0] blinkmask_r, en_r, segdata_r;
 	
 	generate
-		if (DATA_WIDTH == 8)
-		begin
+		if (DATA_WIDTH == 8) begin
 			assign internal_byteenable = 1'b1;
-		end
-		else
-		begin
+		end else begin
 			assign internal_byteenable = slave_byteenable;
 		end
 	endgenerate
@@ -124,7 +110,6 @@ module slave_template (
 
     assign user_dataout_0 = segdata_r;
     assign user_dataout_1 = en_r;
-
 	assign address_decode[0] = (slave_address == 4'b0000) & slave_write;
 	assign address_decode[1] = (slave_address == 4'b0001) & slave_write;
 	assign address_decode[2] = (slave_address == 4'b0010) & slave_write;
@@ -138,31 +123,26 @@ module slave_template (
 	assign address_bank_decode[1] = (address_decode_d1[7:4] != 0)? 1'b1 : 1'b0;
 	assign address_bank_decode[2] = (address_decode_d1[11:8] != 0)? 1'b1 : 1'b0;
 	assign address_bank_decode[3] = (address_decode_d1[15:12] != 0)? 1'b1 : 1'b0;
-		
 	
-	always @ (posedge clk or posedge reset)
-	begin
-		if (reset == 1)
-		begin
+	always @ (posedge clk or posedge reset) begin
+		if (reset == 1) begin
 			slave_read_d1 <= 0;
 			slave_read_d2 <= 0;
 			slave_write_d1 <= 0;
 			address_decode_d1 <= 0;
 			address_bank_decode_d1 <= 0;
 			internal_byteenable_d1 <= 0;
-		end
-		else
-		begin
+		end else begin
 			slave_read_d1 <= slave_read;
 			slave_read_d2 <= slave_read_d1;
 			slave_write_d1 <= slave_write;
 			internal_byteenable_d1 <= internal_byteenable;
-			if((slave_read == 1) | (slave_write == 1))
-			begin
+
+			if((slave_read == 1) | (slave_write == 1)) begin
 				address_decode_d1 <= address_decode;
 			end
-			if(slave_read_d1 == 1)
-			begin
+
+			if(slave_read_d1 == 1) begin
 				address_bank_decode_d1 <= address_bank_decode;
 			end
 		end
@@ -185,12 +165,8 @@ module slave_template (
 endmodule
 
 module register_with_bytelanes (clk, reset, data_in, write, byte_enables, data_out);
-
 	parameter DATA_WIDTH = 8;
-	
-	input clk;
-	input reset;
-	
+	input clk, reset;
 	input [7:0] data_in;
 	input write;
 	input [0:0] byte_enables;
@@ -202,19 +178,14 @@ module register_with_bytelanes (clk, reset, data_in, write, byte_enables, data_o
 		begin: register_bytelane_generation	
 			always @ (posedge clk or posedge reset)
 			begin
-				if(reset == 1)
-				begin
+				if (reset == 1)
 					data_out[(LANE*8)+7:(LANE*8)] <= 0;
-				end
-				else
-				begin
-					if((byte_enables[LANE] == 1) & (write == 1))
-					begin
+				else if((byte_enables[LANE] == 1) & (write == 1))
 						data_out[(LANE*8)+7:(LANE*8)] <= data_in[(LANE*8)+7:(LANE*8)];
-					end
-				end
 			end
 		end
 	endgenerate
-	
 endmodule
+
+
+
