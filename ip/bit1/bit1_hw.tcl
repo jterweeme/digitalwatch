0ce538c64344534366773da8bb49f0a8a96f41ec
Jasper ter Weeme <jasper@nbalternate.lan>
2014-06-24 17:40:51 +0200

    backup

--- ip/bit1/bit1_hw.tcl
+++ ip/bit1/bit1_hw.tcl
@@ -50,6 +50,7 @@ add_interface_port          s1 avs_s1_writedata writedata Input 32
 add_interface_port          s1 avs_s1_address address Input 2
 add_interface_port          s1 avs_s1_read read Input 1
 add_interface_port          s1 avs_s1_readdata readdata Output 32
+add_interface_port          s1 chipselect chipselect Input 1
 set_interface_assignment    s1 embeddedsw.configuration.isFlash 0
 set_interface_assignment    s1 embeddedsw.configuration.isMemoryDevice 0
 set_interface_assignment    s1 embeddedsw.configuration.isNonVolatileStorage 0
--- ip/bit1/bit1top.v
+++ ip/bit1/bit1top.v
@@ -1,25 +1,57 @@
 module bit1top(
+    input [2:0] avs_s1_address,
+    input chipselect,
     input csi_clk,
     input csi_reset,
-    input [1:0] avs_s1_address,
-    input avs_s1_read,
-    output wire [31:0] avs_s1_readdata,
     input avs_s1_write,
     input [31:0] avs_s1_writedata,
-    output wire coe_bit
+    inout wire coe_bit,
+    output reg [31:0] avs_s1_readdata
 );
+    wire clk_en;
+    reg data_dir;
+    wire data_in;
     reg data_out;
     wire read_mux_out;
-    assign read_mux_out = {1 {(avs_s1_address == 0)}} & data_out;
-    assign avs_s1_readdata = {32'b0 | read_mux_out};
-    assign coe_bit = data_out;
+    wire wr_strobe;
+    assign clk_en = 1;
+
+    assign read_mux_out = ({1 {(avs_s1_address == 0)}} &
+        data_in) | ({1 {(avs_s1_address == 1)}} & data_dir);
+
+    assign wr_strobe = chipselect && avs_s1_write;
+    assign bidir_port = data_dir ? data_out : 1'bZ;
+    assign data_in = bidir_port;
 
     always @(posedge csi_clk or posedge csi_reset) begin
         if (csi_reset)
-            data_out <= 1'b0;
-        else if (avs_s1_write)
-            data_out <= avs_s1_writedata[0];
+            avs_s1_readdata <= 0;
+        else if (clk_en)
+            avs_s1_readdata <= {32'b0 | read_mux_out};
     end
+
+    always @(posedge csi_clk or posedge csi_reset) begin
+        if (csi_reset)
+            data_out <= 0;
+        else if (clk_en)
+            if (wr_strobe)
+                data_out <= (avs_s1_address == 5) ? data_out & ~avs_s1_writedata :
+                    (avs_s1_address == 4) ? data_out | avs_s1_writedata :
+                    (avs_s1_address == 0)? avs_s1_writedata : data_out;
+    end
+
+    always @(posedge csi_clk or posedge csi_reset) begin
+        if (csi_reset)
+            data_dir <= 0;
+        else if (chipselect && ~avs_s1_write && (avs_s1_address == 1))
+            data_dir <= avs_s1_writedata;
+    end
+
+
+
 endmodule
+
+
+
 
 
