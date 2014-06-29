4ae214b724a84fc3b8cd7ce18fa331482a35912f
Jasper ter Weeme <jasper@nbalternate.lan>
2014-06-29 23:57:08 +0200

    backup

---
+++ ip/i2cbus1/i2cbus1_hw.tcl
@@ -0,0 +1,69 @@
+# Jasper ter Weeme
+
+package require -exact qsys 13.1
+
+source ../common.tcl
+
+mymodule i2cbus1 i2cbus1top
+
+add_fileset_file i2cbus1top.v VERILOG PATH i2cbus1top.v TOP_LEVEL_FILE
+
+add_interface               clock clock end
+set_interface_property      clock clockRate 0
+set_interface_property      clock ENABLED true
+set_interface_property      clock EXPORT_OF ""
+set_interface_property      clock PORT_NAME_MAP ""
+set_interface_property      clock SVD_ADDRESS_GROUP ""
+add_interface_port          clock clk clk Input 1
+
+add_interface               reset reset end
+set_interface_property      reset associatedClock clock
+set_interface_property      reset synchronousEdges DEASSERT
+set_interface_property      reset ENABLED true
+set_interface_property      reset EXPORT_OF ""
+set_interface_property      reset PORT_NAME_MAP ""
+set_interface_property      reset SVD_ADDRESS_GROUP ""
+add_interface_port          reset reset_n reset_n Input 1
+
+add_interface               s1 avalon end
+set_interface_property      s1 addressUnits WORDS
+set_interface_property      s1 associatedClock clock
+set_interface_property      s1 associatedReset reset
+set_interface_property      s1 bitsPerSymbol 8
+set_interface_property      s1 burstOnBurstBoundariesOnly false
+set_interface_property      s1 burstcountUnits WORDS
+set_interface_property      s1 explicitAddressSpan 0
+set_interface_property      s1 holdTime 0
+set_interface_property      s1 linewrapBursts false
+set_interface_property      s1 maximumPendingReadTransactions 0
+set_interface_property      s1 readLatency 0
+set_interface_property      s1 readWaitTime 1
+set_interface_property      s1 setupTime 0
+set_interface_property      s1 timingUnits Cycles
+set_interface_property      s1 writeWaitTime 0
+set_interface_property      s1 ENABLED true
+set_interface_property      s1 EXPORT_OF ""
+set_interface_property      s1 PORT_NAME_MAP ""
+set_interface_property      s1 SVD_ADDRESS_GROUP ""
+add_interface_port          s1 write_n write_n Input 1
+add_interface_port          s1 writedata writedata Input 8
+add_interface_port          s1 address address Input 3
+add_interface_port          s1 readdata readdata Output 8
+add_interface_port          s1 chipselect chipselect Input 1
+set_interface_assignment    s1 embeddedsw.configuration.isFlash 0
+set_interface_assignment    s1 embeddedsw.configuration.isMemoryDevice 0
+set_interface_assignment    s1 embeddedsw.configuration.isNonVolatileStorage 0
+set_interface_assignment    s1 embeddedsw.configuration.isPrintableDevice 0
+
+add_interface               conduit_end conduit end
+set_interface_property      conduit_end associatedClock clock
+set_interface_property      conduit_end associatedReset reset
+set_interface_property      conduit_end ENABLED true
+set_interface_property      conduit_end EXPORT_OF ""
+set_interface_property      conduit_end PORT_NAME_MAP ""
+set_interface_property      conduit_end SVD_ADDRESS_GROUP ""
+add_interface_port          conduit_end bidir_port export Bidir 1
+add_interface_port          conduit_end coe_clk export Output 1
+
+
+
---
+++ ip/i2cbus1/i2cbus1top.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+module i2cbus1top (
+    input [2:0] address,
+    input chipselect,
+    input clk,
+    input reset_n,
+    input write_n,
+    input [7:0] writedata,
+    inout wire bidir_port,
+    output reg coe_clk,
+    output reg [7:0] readdata
+);
+    wire clk_en, data_in, read_mux_out, wr_strobe;
+    reg data_dir, data_out;
+    assign read_mux_out = ({1 {(address == 0)}} & data_in) | ({1 {(address == 1)}} & data_dir);
+    assign wr_strobe = chipselect && ~write_n;
+    assign bidir_port = data_dir ? data_out : 1'bZ;
+    assign data_in = bidir_port;
+
+    always @(posedge clk or negedge reset_n)
+        readdata <= ~reset_n ? 0 : {8'b0 | read_mux_out};
+
+    always @(posedge clk or negedge reset_n) begin
+        if (reset_n == 0) begin
+            data_out <= 0;
+            coe_clk <= 0;
+        end else if (wr_strobe) begin
+            data_out <= address == 0 ? writedata : data_out;
+            coe_clk <= address == 2 ? writedata : coe_clk;
+        end
+    end
+
+    always @(posedge clk or negedge reset_n) begin
+        if (reset_n == 0)
+            data_dir <= 0;
+        else if (chipselect && ~write_n && (address == 1))
+            data_dir <= writedata;
+    end
+endmodule
+
+
+
+
